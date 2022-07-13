62c73d2802c87a010aa10dbacf9613212f317229
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-07-13 11:46:22 -0300

    Start project and create config test

---
+++ src/main/java/com/dantas/teste/config/TestConfig.java
@@ -0,0 +1,20 @@
+package com.dantas.teste.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+	
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		
+
+	}
+
+}
