0922f5a8d9dc3b92cc7c2934f7b989234a2b92e2
Carlos Alberto Dantas Filho <carlosdantas485@gmail.com>
2022-07-13 13:28:23 -0300

    Create user resources and services - ( GET )

--- src/main/java/com/dantas/teste/services/UserService.java
+++ src/main/java/com/dantas/teste/services/UserService.java
@@ -1,5 +1,8 @@
 package com.dantas.teste.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +14,18 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-
+	
+	
 }
